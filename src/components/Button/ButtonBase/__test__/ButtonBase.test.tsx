a3740b145dd3f7911a86d760dae2fe5947899a1f
binhtt131222000 <binhtt.13122000@gmail.com>
2021-08-09 01:16:55 +0700

    feat: implement testing for app

--- src/components/Button/ButtonBase/__test__/ButtonBase.test.tsx
+++ src/components/Button/ButtonBase/__test__/ButtonBase.test.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
 
-import ButtonBase, { ButtonBaseProps } from "../";
+import ButtonBase, { ButtonBaseProps } from "..";
 
-import { render, screen } from "@testing-library/react";
+import { render, RenderResult, screen } from "@testing-library/react";
 
 describe("<ButtonBase />", () => {
+    let wrapper: RenderResult;
+
     const props: ButtonBaseProps = {
         children: <div>Test</div>,
     };
 
     beforeEach(() => {
-        render(<ButtonBase {...props} />);
+        wrapper = render(<ButtonBase {...props} />);
     });
 
     it("should exist children", () => {
         expect(screen.getByText("Test")).toBeInTheDocument();
+    });
+
+    it("should match snapshot", () => {
+        expect(wrapper.container).toMatchSnapshot();
     });
 });
