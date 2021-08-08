81bb224c3428453ebfe9a8cc234e88ba08a1bc87
binhtt131222000 <binhtt.13122000@gmail.com>
2021-08-08 17:06:10 +0700

    setup storybook

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Header } from "./Header";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+    title: "Example/Header",
+    component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+    user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
